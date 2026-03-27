e7a6d724c752419de09ddd7ce36184cc9bc60afb
Raad Salem <raadsalemrs@gmail.com>
2026-03-27 09:30:21 +0300

    commiting files after merge

--- start.py
+++ start.py
@@ -5,3 +5,9 @@
 print("I Love Python")
 print("I am learning Python")
 print("Python is great for data science")
+
+
+
+
+#This is a new changer for code
+print("I Love merging code and Learning how to use git")
